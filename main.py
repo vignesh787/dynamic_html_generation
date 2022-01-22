01c1063d10a52ba2eb498853ec948e18cb822533
vignesh <vignesh787@gmail.com>
2022-01-22 12:25:27 +0530

    Final Commit Form For Rending Dynamic HTML

--- main.py
+++ main.py
@@ -12,11 +12,41 @@ def generate():
         content = request.form.get('Content')
         image = request.form.get('Image')
         link = request.form.get('Link')
-        return "Got the data"+topic
+        template = f"""
+
+            <div id="container-1">
+            <h1>{ topic }</h1>
+            <h2>{ subTopic } </h2>
+            </div>
+            <div id="container-2">
+            <p id="back"> {content}
+            </p>
+
+            <h3> Image </h3>
+            <img src='static/{image} alt="DS2.jpeg">
+            <p id ="inlinepara">
+            </p></div>
+
+            <div id="container-3">
+            <h3> Additional Information </h3>
+            <a href={link}>More Info Here </a>
+            </div>
+
+
+        """
+
+        final = "{% extends 'base.html' %} {% block main %}" + template + "{% endblock %}"
+
+        file=open(f'templates/{topic}.html', 'w')
+        file.write(final)
+        file.close()
+        return render_template(f'{topic}.html')
     return render_template('add.html')
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', debug = True)
+
+
 
 
 
